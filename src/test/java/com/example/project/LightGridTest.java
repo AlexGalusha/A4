20536da7f56798fdbfe9e22f01380ac453b15560
AlexGalusha <ag616871@wne.edu>
2025-04-24 21:51:44 +0000

    code:  on and return state

--- src/test/java/com/example/project/LightGridTest.java
+++ src/test/java/com/example/project/LightGridTest.java
@@ -54,3 +54,12 @@ void turnOn_EntireGrid_OneMillionLightsAreLit() {
 
 }
 
+@Test
+void toggle_TwiceOnSameArea_ReturnsToOriginalState() {
+    LightGrid grid = new LightGrid();
+    grid.toggle(100, 100, 200, 200); 
+    assertEquals(0, grid.countLit());
+
+}    
+
+}
